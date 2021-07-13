f6cae752689e67641652f51a39cdce49347047bb
Vadym Stupakov <vadim.stupakov@gmail.com>
2021-07-13 14:11:49 +0300

    wip

--- setup.py
+++ setup.py
@@ -8,19 +8,20 @@ import numpy as np
 # Note that the original compile flags below are GCC flags unsupported by the Visual C++ 2015 build tools.
 # They can safely be removed.
 
-ext_modules = [
-    Extension(
-        'pycocotools._mask',
-        sources=['common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), 'common'],
-        extra_compile_args=[] # originally was ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
-    )
-]
+if __name__ == '__main__':
+    ext_modules = [
+        Extension(
+            'pycocotools._mask',
+            sources=['common/maskApi.c', 'pycocotools/_mask.pyx'],
+            include_dirs = [np.get_include(), 'common'],
+            extra_compile_args=[] # originally was ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        )
+    ]
 
-setup(name='pycocotools',
-      packages=['pycocotools'],
-      package_dir = {'pycocotools': 'pycocotools'},
-      version='2.0',
-      ext_modules=
-          cythonize(ext_modules)
-      )
+    setup(name='pycocotools',
+          packages=['pycocotools'],
+          package_dir = {'pycocotools': 'pycocotools'},
+          version='2.0',
+          ext_modules=
+              cythonize(ext_modules)
+          )
